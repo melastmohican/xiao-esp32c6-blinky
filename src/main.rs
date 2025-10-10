715b414efec1cdfe613ca21c7b94deb3d9036249
Patrik Husfloen <redoz@redoz.com>
2025-10-10 20:29:37 +0200

    Update to latest packages

--- src/main.rs
+++ src/main.rs
@@ -4,32 +4,33 @@
 //! - LED => GPIO15
 //! https://github.com/espressif/arduino-esp32/blob/d47771f2cc649c3cd52a3f6eb3d9b97c82005ffb/variants/XIAO_ESP32C6/pins_arduino.h#L13
 
-
 #![no_std]
 #![no_main]
 
 use esp_backtrace as _;
 use esp_hal::{
+    main,
     delay::Delay,
-    gpio::{Io, Level, Output},
-    prelude::*,
+    gpio::{Level, Output},
+    time::Duration,
+    gpio::OutputConfig
 };
 
-#[entry]
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[main]
 fn main() -> ! {
     let peripherals = esp_hal::init(esp_hal::Config::default());
 
     // Set GPIO0 as an output, and set its state high initially.
-    let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
-    let mut led = Output::new(io.pins.gpio15, Level::High);
-
+    let mut led = Output::new(peripherals.GPIO15, Level::High, OutputConfig::default());
     let delay = Delay::new();
 
     loop {
         led.toggle();
-        delay.delay_millis(500);
+        delay.delay_millis(250);
         led.toggle();
-        // or using `fugit` duration
-        delay.delay(2.secs());
+        // or using  duration
+        delay.delay(Duration::from_secs(1));
     }
 }
